901ade4328d7d5317c07b5c36951804b93d04932
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2021-02-13 19:36:33 +0530

    CSS Issue solved using Bootstrap

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -12,9 +12,14 @@ import Header from './header.js';
 import Features from './features';
 import About from './about';
 
-
 import Contact from './contact';
 import JsonData from './../../data/data.json';
+import "../../public/css/nivo-lightbox/nivo-lightbox.css";
+import "../../public/css/nivo-lightbox/default.css";
+import "../../public/fonts/font-awesome/css/font-awesome.css";
+import "../../public/css/style.css";
+// import "../../public/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 import './App.css';
 
