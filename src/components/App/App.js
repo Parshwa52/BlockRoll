24d140c90a0d169872ad8e16b5619be6f536aee3
RudreshVeerkhare <veerkharerudresh@gmail.com>
2021-02-13 11:59:06 +0530

    Dashboard Template added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import './App.css';
-import {Switch as Router_Switch, Route} from "react-router-dom";
+import { Switch as Router_Switch, Route } from "react-router-dom";
 import Test from '../Test/Test';
 import Payroll from '../Payroll/Payroll';
-
+import Dashboard from '../Dashboard/Dashboard';
 
 function App() {
   return (
     <React.Fragment>
-        <Router_Switch>
-            <Route path="/" component = {Payroll} exact />
-            <Route path="/test/" component = {Test} />
-          </Router_Switch>
+      <Router_Switch>
+        <Route path="/" component={Payroll} exact />
+        <Route path="/test/" component={Test} />
+        <Route path="/dash/" component={Dashboard} />
+      </Router_Switch>
     </React.Fragment>
   );
 }
