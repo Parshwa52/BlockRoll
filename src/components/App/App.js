b1d2230b28822b3ccb76298960a8e81405208da8
RudreshVeerkhare <veerkharerudresh@gmail.com>
2021-02-13 13:18:04 +0530

    Forms added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { Switch as Router_Switch, Route } from "react-router-dom";
 import Test from '../Test/Test';
 import Payroll from '../Payroll/Payroll';
 import Dashboard from '../Dashboard/Dashboard';
+import Checkout from '../Forms/Checkout';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
         <Route path="/" component={Payroll} exact />
         <Route path="/test/" component={Test} />
         <Route path="/dash/" component={Dashboard} />
+        <Route path="/payroll/" component={Checkout} />
       </Router_Switch>
-      </React.Fragment>
+    </React.Fragment>
   );
 }
 
