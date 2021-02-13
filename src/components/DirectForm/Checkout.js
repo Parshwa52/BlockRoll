1bf66c639889417429de9933802881fc4da1022a
RudreshVeerkhare <veerkharerudresh@gmail.com>
2021-02-13 18:02:48 +0530

    Direct Send form added

--- src/components/DirectForm/Checkout.js
+++ src/components/DirectForm/Checkout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -10,21 +10,11 @@ import StepLabel from "@material-ui/core/StepLabel";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
-import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import Review from "./Review";
 
 // custom imports
 import { useAddress } from "./utils";
-
-const INIT_DATA = {
-    payrate: undefined,
-    duration: undefined,
-    leaves: undefined,
-    leavecost: undefined,
-    delayeddays: undefined,
-    delaycostperday: undefined,
-};
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
@@ -63,14 +53,13 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const steps = ["Payment details", "Payroll Details", "Transaction Summary"];
+const steps = ["Payment details", "Transaction Summary"];
 
 export default function Checkout() {
     const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState(0);
-    // add data sates here
-    const [formData, setFormData] = React.useState(INIT_DATA);
+    const [activeStep, setActiveStep] = useState(0);
     const [username, address, setUsername] = useAddress("");
+    const [amount, setAmount] = useState();
 
     const handleNext = () => {
         if (activeStep == 0 && address === "") {
@@ -92,12 +81,18 @@ export default function Checkout() {
                         username={username}
                         address={address}
                         setUsername={setUsername}
+                        amount={amount}
+                        setAmount={setAmount}
                     />
                 );
             case 1:
-                return <AddressForm />;
-            case 2:
-                return <Review />;
+                return (
+                    <Review
+                        amount={amount}
+                        username={username}
+                        address={address}
+                    />
+                );
             default:
                 throw new Error("Unknown step");
         }
--- src/components/DirectForm/Review.js
+++ src/components/DirectForm/Review.js
@@ -5,49 +5,10 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Grid from "@material-ui/core/Grid";
-import {
-    getpayRate,
-    getDuration,
-    getOriginalPayment,
-    getLeavesCost,
-    getDelayCost,
-} from "./utils";
+import { getCurrUsername, getCurrAcc } from "./utils";
 
 // This is the blockchain object we receive using web3
 const APIobject = 100;
-
-const products = [
-    {
-        name: "PayRate",
-        desc: "Payment Rate",
-        color: "green",
-        price: getpayRate(APIobject),
-    },
-    {
-        name: "Duration",
-        desc: "Duration of Task",
-        color: "green",
-        price: getDuration(APIobject),
-    },
-    {
-        name: "OriginalPayment",
-        desc: "Payment",
-        color: "green",
-        price: getOriginalPayment(APIobject),
-    },
-    {
-        name: "Leaves Cost",
-        desc: "Amount deducted for leaves",
-        color: "red",
-        price: getLeavesCost(APIobject),
-    },
-    {
-        name: "Delay Cost",
-        desc: "Amount deducted for delays",
-        color: "red",
-        price: getDelayCost(APIobject),
-    },
-];
 
 const addresses = [
     "1 Material-UI Drive",
@@ -55,12 +16,6 @@ const addresses = [
     "Anytown",
     "99999",
     "USA",
-];
-const payments = [
-    { name: "Card type", detail: "Visa" },
-    { name: "Card holder", detail: "Mr John Smith" },
-    { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
-    { name: "Expiry date", detail: "04/2024" },
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -99,8 +54,28 @@ const DesiredTypography = (props) => {
     return result;
 };
 
-export default function Review() {
+export default function Review({ username, amount, address }) {
     const classes = useStyles();
+
+    const products = [
+        {
+            name: "BlockRolls",
+            desc: "Amount of BlockRoll transferred",
+            color: "red",
+            price: amount,
+        },
+        {
+            name: "Trasaction Fees",
+            desc: "Amount deducted for transaction",
+            color: "red",
+            price: "0",
+        },
+    ];
+
+    const payments = [
+        { name: "Reciever", detail: username },
+        { name: "Reciever Account", detail: address },
+    ];
 
     return (
         <React.Fragment>
@@ -124,23 +99,12 @@ export default function Review() {
                 <ListItem className={classes.listItem}>
                     <ListItemText primary="Total" />
                     <Typography variant="subtitle1" className={classes.total}>
-                        $34.06
+                        {amount}
                     </Typography>
                 </ListItem>
             </List>
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                    <Typography
-                        variant="h6"
-                        gutterBottom
-                        className={classes.title}
-                    >
-                        Shipping
-                    </Typography>
-                    <Typography gutterBottom>John Smith</Typography>
-                    <Typography gutterBottom>{addresses.join(", ")}</Typography>
-                </Grid>
-                <Grid item container direction="column" xs={12} sm={6}>
+                <Grid item container direction="column" xs={12}>
                     <Typography
                         variant="h6"
                         gutterBottom
@@ -157,7 +121,7 @@ export default function Review() {
                                     </Typography>
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <Typography gutterBottom>
+                                    <Typography noWrap={true} gutterBottom>
                                         {payment.detail}
                                     </Typography>
                                 </Grid>
